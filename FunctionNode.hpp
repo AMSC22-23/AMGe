6eb1e4d7601dc4add53395e940dd88871d07dacb
Sterning171177 <giuseppe.starnini@mail.polimi.it>
2023-11-17 18:03:26 +0000

    another fix

--- FunctionNode.hpp
+++ FunctionNode.hpp
@@ -1,7 +1,13 @@
 template<class T>
+
 class FunctionNode {		//abstract class for mapping
+
+
 public:
     FunctionNode() = default;
+
     virtual T getValue(int i,int j)  = 0;
+
     virtual ~FunctionNode() = default;
+
 };

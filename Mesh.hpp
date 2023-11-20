28108ac52622fa8155dd78f2a51e55258afe30a9
Sterning171177 <giuseppe.starnini@mail.polimi.it>
2023-11-20 17:34:40 +0000

    fixing mesh

--- Mesh.hpp
+++ Mesh.hpp
@@ -4,34 +4,39 @@
 
 class Mesh : public FunctionNode<Point> {   //map from mesh to omega
 public:
-    Mesh(double _x, double _y, double _width, double _height, unsigned int _N) : FunctionNode() {
+    Mesh(double _x, double _y, double _width, double _height, unsigned int _Nx, unsigned int _Ny) : FunctionNode() {
 	x      = _x;
 	y      = _y;
 	width  = _width;
 	height = _height;
-	N      = _N;
+	Nx     = _Nx;
+    Ny     = _Ny;
     };
 
 
     Point getValue(int i, int j) override {
 	return Point(
-	     x + i * width  / N
-	    ,y + j * height / N
+	     x + i * getDiscretizationStepX()
+	    ,y + j * getDiscretizationStepY()
 	);
     }
 
 
-    unsigned int getDimension () {
-	return N;
+    unsigned int getDimensionX () {
+	return Nx;
+    }
+
+    unsigned int getDimensionY () {
+	return Ny;
     }
 
 
     double getDiscretizationStepX() {
-	return width / static_cast<double>(N);
+	return width / static_cast<double>(Nx);
     }
 
     double getDiscretizationStepY() {
-	return height / static_cast<double>(N);
+	return height / static_cast<double>(Ny);
     }
 
     double getWidth(){
@@ -55,6 +60,6 @@ public:
 
 
 private:
-    unsigned int N;
+    unsigned int Nx, Ny;
     double x,y,width,height;
 };
--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-#define N 50
+#define Nx 50
+#define Ny 50
 #define N_INTERNAL (N-2)
 
 
@@ -27,7 +28,7 @@ void print_vector(double b[N_INTERNAL*N_INTERNAL]) {
 
 
 int index(int i, int j) {
-	return i * N + j;
+	return i * Ny + j;
 }
 
 
@@ -48,9 +49,9 @@ double f(double x,double y){  //main function
 
 
 void setup_solution(Mesh &M, vector<double> &U, ComputeFunctionNode &b) {  //initialize function U to border value
-	for (int i = 0; i < M.getDimension(); ++i) {
-		for (int j = 0; j < M.getDimension(); ++j) {
-			U[i * M.getDimension() + j] = b.getValue(i, j);
+	for (int i = 0; i < M.getDimensionY(); ++i) {
+		for (int j = 0; j < M.getDimensionX(); ++j) {
+			U[i * M.getDimensionY() + j] = b.getValue(i, j);
 		}
 	}
 }
@@ -58,8 +59,8 @@ void setup_solution(Mesh &M, vector<double> &U, ComputeFunctionNode &b) {  //ini
 void jacobi_iteration(double h_square, vector<double> &U_old, vector<double> &U, ComputeFunctionNode &function,Mesh m){  //1 cycle of Jacobi
 	double b;
 
-	for (int i = 1; i < m.getDimension()-1; ++i) {
-			for (int j = 1; j < m.getDimension()-1; ++j) {     //iterating over nodes and computing for each one a row of matrix A
+	for (int i = 1; i < m.getDimensionY()-1; ++i) {
+			for (int j = 1; j < m.getDimensionX()-1; ++j) {     //iterating over nodes and computing for each one a row of matrix A
 				
 				b = function.getValue(i, j) * h_square;
 
@@ -84,8 +85,8 @@ double compute_norm(vector<double> &residual){   //euclidean norm
 
 
 double compute_residual(double h_square, vector<double> &U, vector<double> &residual, ComputeFunctionNode &function){  
-	for (int i = 1; i < N-1; ++i) {
-			for (int j = 1; j < N-1; ++j) { 
+	for (int i = 1; i < Ny-1; ++i) {
+			for (int j = 1; j < Nx-1; ++j) { 
 					residual[index(i,j)]=h_square*function.getValue(i,j)+U[index(i,j-1)]+U[index(i-1,j)]-4*U[index(i,j)]+U[index(i+1,j)]+U[index(i,j+1)];
 			}
 	}
@@ -95,15 +96,15 @@ double compute_residual(double h_square, vector<double> &U, vector<double> &resi
 
 
 int main (int argc, char *argv[]) {
-	Mesh mesh(-1.0, 0.0, 2.0, 2.0, N);
+	Mesh mesh(-1.0, 0.0, 2.0, 2.0, Nx,Ny);
 	ComputeFunctionNode bordo(&mesh,g);
 	ComputeFunctionNode funzione(&mesh,f);
 
 
-	vector<double> U_old(mesh.getDimension()*mesh.getDimension());
-	vector<double> U(mesh.getDimension()*mesh.getDimension());
+	vector<double> U_old(mesh.getDimensionX()*mesh.getDimensionY());
+	vector<double> U(mesh.getDimensionX()*mesh.getDimensionY());
 	
-	vector<double> residual(mesh.getDimension()*mesh.getDimension());
+	vector<double> residual(mesh.getDimensionX()*mesh.getDimensionY());
 	double residual_norm=100;
 
 	int count =0;

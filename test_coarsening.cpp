9048b23031364ce6e8de9a303949b296a3f228ac
Sterning171177 <giuseppe.starnini@mail.polimi.it>
2023-12-11 14:58:30 +0000

    print restriction and interpolation of a residual

--- test_coarsening.cpp
+++ test_coarsening.cpp
@@ -1,59 +1,72 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include "LatticeMesh.hpp"
+#include "Lattice.hpp"
 #include "Utils.hpp"
 
 
 using namespace std;
 
 
+void residual(Lattice &mesh, const std::vector<double> &u, const std::vector<double> &b, std::vector<double> &r) {
+	for (Index i : mesh.get_inner_nodes()) {
+		const auto [nord, sud, ovest, est] = mesh.get_cardinal_neighbours(i);
+
+		r[i] = 4.0 * u[i] - u[nord] - u[sud] - u[ovest] - u[est] - b[i];
+	}
+}
+
+
+void set_initial_guess(Lattice &mesh, std::vector<double> &u, double (*g)(double x, double y)) {
+	mesh.evaluate_function(u, g);
+
+	for (Index i : mesh.get_inner_nodes()) {
+		u[i] = 0.0;
+	}
+}
+
+
+double g(double x,double y) {
+	return x * x * x * x * x / 20.0 + y * y * y * y * y / 20.0;
+}
+
+
+double f(double x,double y) {
+	return -(x * x * x + y * y * y);
+}
+
+
 
 int main (int argc, char *argv[]) {
-	const int Nx = 5;
-	const int Ny = 5;
+	const int N = 5;
 
 
-	LatticeMesh mesh(0.0, 0.0, 1.0, 1.0, Nx,Ny);
-	LatticeMesh coarse = mesh.build_coarse();
+	Lattice fine(0.0, 0.0, 1.0, 1.0, N);
+	Lattice coarse = fine.build_coarse();
 
 
-	std::vector<double> u_fine(Nx*Ny);
-	std::vector<double> u_interpolated(Nx*Ny);
-	std::vector<double> u_coarse((1+Nx/2) * (1+Ny/2));
+	std::vector<double> u_fine(fine.numel());
+	std::vector<double> r(fine.numel());
+	std::vector<double> b(fine.numel());
+	std::vector<double> r_interpolated(fine.numel());
+	std::vector<double> r_coarse(coarse.numel());
 
 
-	for (int i = 0; i < u_fine.size(); ++i) {
-		u_fine[i] = i;
+	set_initial_guess(fine, u_fine, g);
+	fine.evaluate_forcing_term(b, f);
+	for (Index i : fine.get_inner_nodes()) {
+		u_fine[i] = 0.0;
 	}
 
 
-	mesh.project_on_coarse(u_fine, u_coarse);
-	mesh.interpolate_on_fine(coarse, u_interpolated, u_coarse);
+	residual(fine,u_fine, b, r);
+	fine.print_vector(r, "residual");
 
+	fine.project_on_coarse(fine, r, r_coarse);
+	coarse.print_vector(r_coarse, "residual_coarse");
 
-	std::cout << "fine" << std::endl;
-	for (auto x : u_fine) {
-		std::cout << x << std::endl;
-	}
-
-
-	std::cout << std::endl;
-
-
-	std::cout << "coarse" << std::endl;
-	for (auto x : u_coarse) {
-		std::cout << x << std::endl;
-	}
-
-
-	std::cout << std::endl;
-
-
-	std::cout << "interpolated" << std::endl;
-	for (auto x : u_interpolated) {
-		std::cout << x << std::endl;
-	}
+	fine.interpolate_on_fine(coarse, r_interpolated, r_coarse);
+	fine.print_vector(r_interpolated, "r_interpolated");
 
 
 	return 0;

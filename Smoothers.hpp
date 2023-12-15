2ece03d77bb51674a32ca81f2bb8463ba5941cad
FattiMei <teomei68@gmail.com>
2023-12-15 14:34:58 +0100

    Added guidelines for openmp parallelization

--- Smoothers.hpp
+++ Smoothers.hpp
@@ -7,6 +7,7 @@
 
 
 void residual(Lattice &mesh, const std::vector<double> &u, const std::vector<double> &b, std::vector<double> &r) {
+	// @TODO: add also parallelization here
 	for (Index i : mesh.get_inner_nodes()) {
 		const auto [nord, sud, ovest, est] = mesh.get_cardinal_neighbours(i);
 
@@ -25,7 +26,7 @@ void gseidel(Lattice &mesh, std::vector<double> &u, const std::vector<double> &b
 
 
 void jacobi_parallel(Lattice &mesh, std::vector<double> &u, const std::vector<double> &old, const std::vector<double> &b) {
-	// occhio al bordo
+	// sembra che openmp non capisca bene i range based loop di c++11, quindi bisogna adattare il for a qualcosa di tradizionale (for(int i = 0; ...))
 	#pragma omp parallel for
 
 	for (Index i : mesh.get_inner_nodes()) {
@@ -38,15 +39,12 @@ void jacobi_parallel(Lattice &mesh, std::vector<double> &u, const std::vector<do
 
 
 void jacobi(Lattice &mesh, std::vector<double> &u, const std::vector<double> &old, const std::vector<double> &b) {
-	// occhio al bordo
-	
 	for (Index i : mesh.get_inner_nodes()) {
 		const auto [nord, sud, ovest, est] = mesh.get_cardinal_neighbours(i);
 
 		u[i] = 0.25 * (b[i] + old[nord] + old[sud] + old[ovest] + old[est]);
 	}
 }
-
 
 
 #endif // !__SMOOTHERS_HPP__

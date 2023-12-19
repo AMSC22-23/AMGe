82d754cb08603926664d3a8729c4803f8b8c2bce
FattiMei <teomei68@gmail.com>
2023-12-19 16:14:59 +0100

    First instance of hpp to cpp convergence

---
+++ src/Utils.cpp
@@ -0,0 +1,60 @@
+#include "Utils.hpp"
+#include <cmath>
+#include <bitset>
+
+
+double norm(const std::vector<double> &v){
+	double result = 0.0;
+
+	for (double x : v) {
+		result += x * x;
+	}
+
+	return std::sqrt(result);
+}
+
+
+void export_to_matlab(const std::string &name, const std::vector<double> &v, std::ostream &out){
+	out
+		<< "# name: "      << name     << std::endl
+		<< "# type: matrix"            << std::endl
+		<< "# rows: 1"                 << std::endl
+		<< "# columns: "   << v.size() << std::endl;
+
+
+	for (auto x : v) {
+		out << x << ' ';
+	}
+
+
+	out << '\n' << '\n';
+}
+
+
+bool is_2nplusone(int n){
+	int count=0;
+	std::bitset<64> bits(n);
+	
+	if(bits.test(0)==0){
+		return false;
+	}
+
+	for (int i=1;i<64;i++){
+		
+		if(bits.test(i)==1){
+			count++;
+			
+			if(count==2){
+				return false;
+			}
+
+		}
+	
+	}
+
+	if(count==0){
+		return false;
+	}
+	
+	return true;
+}

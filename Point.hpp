06f522398efa57292262283b5a11721ad4e06685
FattiMei <teomei68@gmail.com>
2023-12-19 14:47:09 +0100

    Refactor on Point class, but since none of the code uses the functionality it will be soon removed

--- Point.hpp
+++ Point.hpp
@@ -1,16 +1,12 @@
 #ifndef __POINT_HPP__
 #define __POINT_HPP__
 
-//@note: if everything is public just go with a `struct`
+
 //@note: (minor) is you used an std::array could easily generalize to 3D
-class Point {
-public:
-    Point(double _x, double _y) {
-	x = _x;
-	y = _y;
-    }
+//@todo: (future) template this struct for dealing with problems in 1D, 2D, 3D
 
 
+struct Point {
     double x,y;
 };
 

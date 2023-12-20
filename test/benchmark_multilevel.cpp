fad8296fc7215acf22577fcdc273389e1a753a2f
FattiMei <teomei68@gmail.com>
2023-12-20 08:43:36 +0100

    Added even more levels to the benchmark

--- test/benchmark_multilevel.cpp
+++ test/benchmark_multilevel.cpp
@@ -79,7 +79,69 @@ static void BM_FourLevel(benchmark::State& state) {
 }
 
 
+static void BM_FiveLevel(benchmark::State& state) {
+	int n = state.range(0);
+	Lattice fine(0.0, 0.0, 1.0, 1.0, n+1);
+	Multigrid solver(fine, PRE_SMOOTHING_STEPS, POST_SMOOTHING_STEPS, 5);
+
+
+	std::vector<double> u(fine.numel());
+	std::vector<double> b(fine.numel());
+
+
+	fine.evaluate_boundary_conditions(u, g);
+	fine.evaluate_forcing_term(b, f);
+
+
+	for (auto _ : state) {
+		solver.step(u, b);
+	}
+}
+
+
+static void BM_SixLevel(benchmark::State& state) {
+	int n = state.range(0);
+	Lattice fine(0.0, 0.0, 1.0, 1.0, n+1);
+	Multigrid solver(fine, PRE_SMOOTHING_STEPS, POST_SMOOTHING_STEPS, 6);
+
+
+	std::vector<double> u(fine.numel());
+	std::vector<double> b(fine.numel());
+
+
+	fine.evaluate_boundary_conditions(u, g);
+	fine.evaluate_forcing_term(b, f);
+
+
+	for (auto _ : state) {
+		solver.step(u, b);
+	}
+}
+
+
+static void BM_SevenLevel(benchmark::State& state) {
+	int n = state.range(0);
+	Lattice fine(0.0, 0.0, 1.0, 1.0, n+1);
+	Multigrid solver(fine, PRE_SMOOTHING_STEPS, POST_SMOOTHING_STEPS, 7);
+
+
+	std::vector<double> u(fine.numel());
+	std::vector<double> b(fine.numel());
+
+
+	fine.evaluate_boundary_conditions(u, g);
+	fine.evaluate_forcing_term(b, f);
+
+
+	for (auto _ : state) {
+		solver.step(u, b);
+	}
+}
+
 BENCHMARK(BM_TwoLevel)->Arg(32)->Arg(64)->Arg(128)->Arg(256)->Arg(512)->Arg(1024);
 BENCHMARK(BM_ThreeLevel)->Arg(32)->Arg(64)->Arg(128)->Arg(256)->Arg(512)->Arg(1024);
 BENCHMARK(BM_FourLevel)->Arg(32)->Arg(64)->Arg(128)->Arg(256)->Arg(512)->Arg(1024);
+BENCHMARK(BM_FiveLevel)->Arg(32)->Arg(64)->Arg(128)->Arg(256)->Arg(512)->Arg(1024);
+BENCHMARK(BM_SixLevel)->Arg(32)->Arg(64)->Arg(128)->Arg(256)->Arg(512)->Arg(1024);
+BENCHMARK(BM_SevenLevel)->Arg(64)->Arg(128)->Arg(256)->Arg(512)->Arg(1024);
 BENCHMARK_MAIN();
